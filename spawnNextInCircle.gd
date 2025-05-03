896f52299c2facbd696371142e8f44724924465d
MedusaTea <130802982+MedusaTea@users.noreply.github.com>
2025-05-03 11:35:31 -0500

    Create spawnNextInCircle.gd

---
+++ spawnNextInCircle.gd
@@ -0,0 +1,4 @@
+var degreeSpread = 90
+var wrapConstant = 360 / degreeSpread
+var wrapMultiplier = spawned % wrapConstant
+var spawnedOffset = (spawned / totalForFill) + (wrapMultiplier * degreeSpread)
